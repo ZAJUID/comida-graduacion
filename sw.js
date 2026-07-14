8cdf6a810e3f46ff5c8acc5972cbcbada1722fc8
Zajid Cabrera <zajuidcabreraduran@gmail.com>
2026-07-14 12:06:52 -0600

    Actualizar nav, crear carpeta tematica/assets, cambiar tema a morado/azul neon, bump SW a v18

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Comida de Graduación 2026 - PWA
    ============================================ */
 
-const CACHE_NAME = 'graduacion-v17';
+const CACHE_NAME = 'graduacion-v18';
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
